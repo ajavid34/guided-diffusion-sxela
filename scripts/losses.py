75653765349bd20cd2b78464bfb659e0b8785393
Amirhosssein <ahbagheri01@gmail.com>
2025-07-13 10:14:44 +0330

    added ece

--- scripts/losses.py
+++ scripts/losses.py
@@ -527,7 +527,7 @@ class ECE(BaseDivergence):
             indexes = (prob_values > start) & (prob_values <= end)
             acc_bin = acc[indexes].float().mean(dim=1)
             avg_conf = prob_values[indexes].mean(dim=1)
-                                                 )
+
             ece += torch.abs(avg_conf - acc_bin) * indexes.float()
 
 
